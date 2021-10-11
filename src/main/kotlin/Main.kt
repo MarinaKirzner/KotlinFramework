1d52c11401b51133fda2ac4af6a2630ea48224e7
Marina Kirzner <marina.kirzner@minsk.mm.local>
2021-10-11 11:14:41 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,19 @@
+
+class Singleton private constructor() {
+  companion object {
+    private var instance : Singleton? = null
+    fun getInstance() = synchronized(this) {
+      if(instance == null)
+          instance = Singleton()
+        instance
+    }
+  }
+}
+
+fun main() {
+  println(Singleton.getInstance())
+  println(Singleton.getInstance())
+  println(Singleton.getInstance())
+}
+
+
