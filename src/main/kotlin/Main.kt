4e7ac3c5a9ff2b451af3b2e5ef8e6d53e2d1eb7b
marina.kirzner <marina.lobko@gmail.com>
2021-10-13 18:05:21 +0300

    commin 1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,26 @@
+import dataClass.Employees
+import factory.PositionFactory
+import enum.Position
 import singleton.OneObject
 
 fun main() {
-println(OneObject.getInstance())
+  println(OneObject.getInstance())
+  val employeePM = Employees("IDFinance",Position.PM, "Tom")
+  val employeeQA = employeePM.copy(position = Position.QA)
+  val employeeDEV = Employees("IDFinance",Position.DEV, "Bob")
+
+  val name = employeeDEV.component3()
+  println(name)
+
+  println(employeeQA)
+
+  val factory = PositionFactory()
+  factory.makePosition(Position.PM).make()
+  factory.makePosition(Position.QA).make()
+  factory.makePosition(Position.DEV).make()
+
+
+
 }
 
 
