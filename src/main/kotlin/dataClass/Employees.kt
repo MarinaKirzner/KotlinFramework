ee9cc7fc1e168e54ecf9c8aabc75a1dde17786de
marina.kirzner <marina.lobko@gmail.com>
2021-10-19 16:19:20 +0300

    fix: update due to comments

--- src/main/kotlin/dataClass/Employees.kt
+++
@@ -1,9 +0,0 @@
-package dataClass
-
-import enum.PositionType
-
-data class Employees (val company: String = "IDFinance",
-                      val position: PositionType = PositionType.QA,
-                      val name: String = "Tom") {
-
-}
