45495dc8648643449a294dc6a2e9dfbb6d3cc509
marina.kirzner <marina.lobko@gmail.com>
2021-10-28 19:14:53 +0300

    test: System Properties

--- src/test/kotlin/config/provider/ConfigProviderSystemPropertiesTest.kt
+++ src/test/kotlin/config/provider/ConfigProviderSystemPropertiesTest.kt
@@ -9,18 +9,18 @@ import org.junit.jupiter.api.Test
 class ConfigProviderSystemPropertiesTest {
 
   companion object {
-    private const val FORMAT_TYPE_SYSTEM_PROPERTIES = "FILE_SYSTEM_PROPERTIES"
+    private const val FORMAT_TYPE_SYSTEM_PROPERTY = "FILE_SYSTEM_PROPERTIES"
   }
 
   @BeforeEach
   fun setSystemProperties() {
-    System.setProperty(FORMAT_TYPE_SYSTEM_PROPERTIES, FormatType.JSON.name)
+    System.setProperty(FORMAT_TYPE_SYSTEM_PROPERTY, FormatType.JSON.name)
   }
 
   @Test
   fun `Verify getConfig returns config from file`() {
     val expectedObject = Config(myHost = "127.0.0.1", url = "test", login = "myemail@gmail.com", pass = "jhfloeriup")
-    val formatSystemProperties: String = System.getProperty(FORMAT_TYPE_SYSTEM_PROPERTIES)
+    val formatSystemProperties: String = System.getProperty(FORMAT_TYPE_SYSTEM_PROPERTY)
     val actualObject: Config = ConfigProvider().getConfig(FormatType.valueOf(formatSystemProperties))
 
     Assertions.assertEquals(
@@ -32,6 +32,6 @@ class ConfigProviderSystemPropertiesTest {
 
   @AfterEach
   fun deleteSystemProperties() {
-    System.clearProperty(FORMAT_TYPE_SYSTEM_PROPERTIES)
+    System.clearProperty(FORMAT_TYPE_SYSTEM_PROPERTY)
   }
 }
