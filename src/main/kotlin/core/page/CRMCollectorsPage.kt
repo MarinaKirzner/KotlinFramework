7b30530f8abd9e1732fad2d56aa8f52f8391e39c
marina.kirzner <marina.lobko@gmail.com>
2021-12-07 10:48:42 +0300

    block classes in one file page has variables for linking to blocks

---
+++ src/main/kotlin/core/page/CRMCollectorsPage.kt
@@ -0,0 +1,8 @@
+package core.page
+
+class CRMCollectorsPage {
+  val headerBlock: HeaderBlock by lazy { HeaderBlock() }
+  val bodyBlock: BodyBlock by lazy { BodyBlock() }
+  val filterCollectorsBlock: FilterCollectorsBlock by lazy { FilterCollectorsBlock() }
+  val tableCollectorsBlock: TableCollectorsBlock by lazy { TableCollectorsBlock() }
+}
