26c836c9dd7c6caa432a765406f531dfaab52598
marina.kirzner <marina.lobko@gmail.com>
2021-12-09 19:58:22 +0300

    send Get-response to registration endpoint verify cookie AuthUser not empty

--- src/main/kotlin/core/http/interceptor/StatusCodeResponse.kt
+++ src/main/kotlin/core/http/interceptor/StatusCodeResponse.kt
@@ -7,10 +7,13 @@ import java.io.IOException
 
 class StatusCodeResponse() : Interceptor {
 
+  val code200 = 200
+  val code201 = 201
+
   override fun intercept(chain: Interceptor.Chain): Response {
     val request: Request = chain.request()
     val response: Response = chain.proceed(request)
-    if (response.code == 200 || response.code == 201) {
+    if (response.code == code200 || response.code == code201) {
       return response
     }
     throw IOException("Unsuccessful API call")
