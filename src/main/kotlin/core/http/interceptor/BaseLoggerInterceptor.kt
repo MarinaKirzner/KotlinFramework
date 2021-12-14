27a164db69154a75daad2b785323faf1a7ce173d
marina.kirzner <marina.lobko@gmail.com>
2021-12-14 16:02:34 +0300

    read ApplicationConfig in StaticHolderContext create BaseLoggerInterceptor

---
+++ src/main/kotlin/core/http/interceptor/BaseLoggerInterceptor.kt
@@ -0,0 +1,26 @@
+package core.http.interceptor
+
+import okhttp3.Interceptor
+import okhttp3.Request
+import okhttp3.Response
+import okhttp3.internal.toHeaderList
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
+
+class BaseLoggerInterceptor : Interceptor {
+
+  private val logger: Logger = LogManager.getLogger()
+
+  override fun intercept(chain: Interceptor.Chain): Response {
+    val request: Request = chain.request()
+    val response: Response = chain.proceed(request)
+    logger.info(
+      """
+     ${"requestUrl: "}${request.url}
+     ${"Header: "}${response.headers.toHeaderList().joinToString("\n")}
+     ${"StatusCode: "}${response.code}
+    """.trimIndent()
+    )
+    return response
+  }
+}
