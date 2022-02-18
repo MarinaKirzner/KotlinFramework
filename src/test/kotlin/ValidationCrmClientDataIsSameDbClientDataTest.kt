828c0c4af54871e77abec13d631b54b7617a12c9
marina.kirzner <marina.lobko@gmail.com>
2022-02-18 09:16:57 +0300

    information from the client by id

--- src/test/kotlin/ValidationCrmClientDataIsSameDbClientDataTest.kt
+++ src/test/kotlin/ValidationCrmClientDataIsSameDbClientDataTest.kt
@@ -1,22 +1,22 @@
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import services.extention.IpruCrmExtension
-import services.page.IpruCrmClientByIdOptions
-import services.page.IpruCrmClientPageOptions
+import services.page.IpruCrmClientByIdOperations
+import services.page.IpruCrmClientPageOperations
 
 @ExtendWith(IpruCrmExtension::class)
 class ValidationCrmClientDataIsSameDbClientDataTest: BaseTest() {
 
-  private val ipruCrmClientPageOptions: IpruCrmClientPageOptions = IpruCrmClientPageOptions()
-  private val clientType: String = "Физическое лицо"
+  private val ipruCrmClientPageOperations: IpruCrmClientPageOperations = IpruCrmClientPageOperations()
+  private val clientType: String = "Р¤РёР·РёС‡РµСЃРєРѕРµ Р»РёС†Рѕ"
   private val clientId: String = "456"
 
   @Test
   fun `CRM - UI - Validation client main-data in the UI is the same as the client data in the database`() {
-    ipruCrmClientPageOptions.apply {
+    ipruCrmClientPageOperations.apply {
       selectClientTypeByCrmClientsPage(clientType)
       selectClientByIdFromCrmClientsPage(clientId)
     }
-    IpruCrmClientByIdOptions().selectInformationFromCrmClientByIdPage()
+    IpruCrmClientByIdOperations().selectInformationFromCrmClientByIdPage()
   }
 }
