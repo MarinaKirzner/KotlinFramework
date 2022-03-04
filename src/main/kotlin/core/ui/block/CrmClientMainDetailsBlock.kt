4636b42694062506a776bf5f0cf6e1a26919d17e
marina.kirzner <marina.lobko@gmail.com>
2022-03-04 16:04:25 +0300

    fix comments

--- src/main/kotlin/core/ui/block/CrmClientMainDetailsBlock.kt
+++ src/main/kotlin/core/ui/block/CrmClientMainDetailsBlock.kt
@@ -7,9 +7,15 @@ class CrmClientMainDetailsBlock : BaseBlock() {
 
   override val baseBlockLocator: By = By.xpath("//h4[text()='Общая информация']/ancestor::table")
 
+  private fun convertValueInString(field: String, map: Map<String, String>): String {
+    return if (map.filter { it.key == field }.entries.first().value == "Да" ||
+      map.filter { it.key == field }.entries.first().value == "1"
+    ) "true"
+    else "false"
+  }
+
   fun getClientIdMainDetailsFromTable(): Map<String, String> {
-    val crmClientMainDetails: MutableMap<String, String> =
-      getTableValues(baseBlockLocator) as MutableMap<String, String>
+    val crmClientMainDetails: MutableMap<String, String> = getTableValues(baseBlockLocator) as MutableMap<String, String>
     crmClientMainDetails.apply {
       remove("Согласие на ПЭП")
       remove("Согласие на проверку")
@@ -17,12 +23,5 @@ class CrmClientMainDetailsBlock : BaseBlock() {
       put("Менялись ли ФИО?", convertValueInString("Менялись ли ФИО?", this))
     }
     return crmClientMainDetails
-  }
-
-  private fun convertValueInString(field: String, map: Map<String, String>): String {
-    return if (map.filter { it.key == field }.entries.first().value == "Да" ||
-      map.filter { it.key == field }.entries.first().value == "1"
-    ) "true"
-    else "false"
   }
 }
