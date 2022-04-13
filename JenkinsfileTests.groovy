be986070a9599ac96a413204bdd943b4c31f0a49
marina.kirzner <marina.lobko@gmail.com>
2022-04-13 17:00:57 +0300

    fix comments

--- JenkinsfileTests.groovy
+++ JenkinsfileTests.groovy
@@ -1,8 +1,19 @@
+@Library('shared_libs@CI_CD_shared_libs')
+
+def gradleTestTask = "internalTest"
+
+static String startSelectedTestSuite(String gradleTestTask) {
+  return """
+            echo Start internal test
+            gradle $gradleTestTask
+  """
+}
+
 pipeline {
   stages {
     stage('Build Tests') {
       steps {
-
+        sh startSelectedTestSuite(gradleTestTask)
       }
     }
   }
