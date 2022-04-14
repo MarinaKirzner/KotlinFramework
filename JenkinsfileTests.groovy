89e684fbbd705765cf657e588816ab2a2ec3f35a
marina.kirzner <marina.lobko@gmail.com>
2022-04-14 13:04:33 +0300

    fix comments

--- JenkinsfileTests.groovy
+++ JenkinsfileTests.groovy
@@ -3,12 +3,7 @@ def gradleTestTask = "buildTests"
 static String startSelectedTestSuite(String gradleTestTask) {
   return """
             echo Start internal test
-
               gradle $gradleTestTask
-              -Dwebdriver.type=remote\\
-              -Dwebdriver.browser.name=chrome\\
-              -Dwebdriver.browser.headless=true\\
-              -Dwebdriver.browser.chrome=100.0.4896.88\\
   """
 }
 
