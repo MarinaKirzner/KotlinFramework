4807367f1a86b80e767e0a7608a6cc43b601086a
marina.kirzner <marina.lobko@gmail.com>
2022-04-14 18:40:53 +0300

    fix comments

--- JenkinsfileTests.groovy
+++ JenkinsfileTests.groovy
@@ -1,7 +1,7 @@
 static String startSelectedTestSuite() {
   return """
             echo Start internal test
-              gradle clean -i test --tests ApplicationPropertiesTest
+              gradle clean tests
               
   """
 }
