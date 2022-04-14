9ea4b5ab8ce23780861ba11c428586ac98346d84
marina.kirzner <marina.lobko@gmail.com>
2022-04-14 17:08:33 +0300

    fix comments

--- JenkinsfileTests.groovy
+++ JenkinsfileTests.groovy
@@ -25,7 +25,7 @@ pipeline {
 
   post {
     always {
-      archiveArtifacts fingerprint: true
+      archiveArtifacts 'build/libs/**/*.jar', fingerprint: true
       junit 'build/test-results/**/*.xml'
     }
   }
