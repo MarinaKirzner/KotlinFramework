d1afac052f0bc7d73028b2a33553514c2c6c051c
marina.kirzner <marina.lobko@gmail.com>
2022-04-14 19:49:39 +0300

    fix comments

--- JenkinsfileTests.groovy
+++ JenkinsfileTests.groovy
@@ -27,7 +27,7 @@ pipeline {
       archiveArtifacts artifacts: '**/*.jar', fingerprint: true
       junit 'build/test-results/**/*.xml'
       publishHTML([
-          reportDir            : "build/reports/tests/tests",
+          reportDir            : "build/reports/tests/test",
           reportFiles          : 'index.html',
           reportName           : "Gradle Test Report",
           allowMissing         : true,
